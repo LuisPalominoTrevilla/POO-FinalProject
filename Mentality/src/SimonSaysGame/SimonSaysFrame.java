9b47f0927a5cc2e6bbaef24e378121a8e80453ec
Miguel <miguel_angel_bucio@hotmail.com>
2017-04-30 17:22:04 -0500

    Update SimonSaysFrame.java

--- Mentality/src/SimonSaysGame/SimonSaysFrame.java
+++ Mentality/src/SimonSaysGame/SimonSaysFrame.java
@@ -7,33 +7,37 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import Users.User;
+
 public class SimonSaysFrame extends JFrame{
 	
     private SimonSays parent;
 	private ColorBoard cB;
 	private Score sc;
 	private Secuencia secuencia;
+	private User user;
 	
-	public SimonSaysFrame(SimonSays parent){
+	public SimonSaysFrame(SimonSays parent, User user){
 		super();
 		this.parent = parent;
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setResizable(false);
+		this.user=user;
 		
 		//Inicializar Componentes
 		
 		this.secuencia= new Secuencia();
-		this.sc=new Score();
-		this.cB= new ColorBoard(this.secuencia, this.sc, this.parent);
+		this.sc=new Score(this, this.user);
+		this.cB= new ColorBoard(this.secuencia, this.sc, this.parent, this);
 		this.sc.setColorBoard(this.cB);
 		JPanel jpS= new JPanel();
 		jpS.setPreferredSize(new Dimension(700,75));
 		jpS.setBackground(Color.gray);
 		
-		//Añadir Componentes a los paneles
+		//AÃ±adir Componentes a los paneles
 		jpS.add(this.secuencia);
 		
-		//Añadir Paneles al frame
+		//AÃ±adir Paneles al frame
 		this.add(this.sc,BorderLayout.NORTH);
 		this.add(this.cB);
 		this.add(jpS,BorderLayout.SOUTH);
@@ -45,5 +49,14 @@ public class SimonSaysFrame extends JFrame{
 	public void start(){
 		this.cB.Start();
 	}
+	
+	public void setUser(User user){
+		this.user=user;
+	}
+	
+	public User getUser(){
+		return this.user;
+	}
 
 }
+
